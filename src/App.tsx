d642104ab09eedfccd2699f795f0803c50810f3d
amir <amirmahdidigbari@yahoo.com>
2020-02-20 14:17:36 +0330

    add react-ruter-dom and node-sass to project create folders for project basedon atomic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route, RouteComponentProps } from "react-router-dom";
 
-function App() {
+const HomeScreenPage: React.FC<RouteComponentProps> = lazy(() => import("./templates/homeScreen/HomeScreenPage"));
+
+type RouterProps = {};
+const Fallback: React.FC<{}> = () => {
+    return <div></div>;
+};
+
+const RootRouter: React.FC<RouterProps> = React.memo(() => {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Suspense fallback={<Fallback />}>
+            <Router>
+                <Switch>
+                    <Route path='/' component={HomeScreenPage} />
+                </Switch>
+            </Router>
+        </Suspense>
     );
-}
-
-export default App;
+});
+export default RootRouter;
---
+++ src/templates/homeScreen/HomeScreenPage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+
+import logo from "atoms/assets/logo.svg";
+import "./App.css";
+
+const HomeScreenPage: React.FC<RouteComponentProps> = React.memo(() => {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+});
+export default HomeScreenPage;
